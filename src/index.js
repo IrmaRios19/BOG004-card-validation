8c1bc5432a2c72bfe589f4153cf4fd564f3f0d14
IrmaRios19 <irmarios0115@gmail.com>
2022-02-09 22:22:40 -0500

    botón guardar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener('click',()=>{
+   let name = document.getElementById ("name").value;
+   if (document.getElementById("name", "email", "phone", "direction", "city").value != "" == true){
+      let guardar = alert ( "Hola " + name  + " tu registro se realizó con éxito!" );
+   }
+   else {
+      alert("Diligencia todos los campos para tu registro")
+   }
+})
+
 console.log(validator);
